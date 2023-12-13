5093750078f9fe9b5f0130c2b9245d924cfc6c20
korounganba <korounganba123@gmail.com>
2023-12-13 15:17:21 +0530

    calulator of addition and substraction commited

---
+++ calculator.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	char oper;
+	int no1,no2,result;
+	printf("enter value for no. 1\n");
+	scanf("%d",no1);
+	printf("enter value for no. 2");
+	scanf("%d",no2);
+
+	printf("what operation would you like to operate? \n");
+	printf("1.for addition type a \n 2.for substraction type b");
+	scanf("%c",oper);
+	
+	switch(oper)
+	{
+		case a :
+			result=no1+no2;
+			break;
+		case b :
+			result=no1-no2;
+			break;
+	}
+	return 0;
+}
+
